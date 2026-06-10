db7c73ce42f5a2232e948dd32986bbdb75e25096
M. Sidiq Firdaus <85211445+Sidiq-coder@users.noreply.github.com>
2026-06-10 15:05:48 +0700

    feat: export question tools API

---
+++ packages/question-tools/src/index.ts
@@ -0,0 +1,10 @@
+export { validateQuestionDataset } from "./validator.js";
+export type {
+  CognitiveLevel,
+  Difficulty,
+  OptionKey,
+  QuestionItem,
+  QuestionOptions,
+  ValidationIssue,
+  ValidationResult
+} from "./types.js";
