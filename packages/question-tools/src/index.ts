6c9d9f19a6dafb2881d72d99f808d41802e14cf2
M. Sidiq Firdaus <85211445+Sidiq-coder@users.noreply.github.com>
2026-06-14 09:12:58 +0700

    feat: export topic summary utility

--- packages/question-tools/src/index.ts
+++ packages/question-tools/src/index.ts
@@ -1,7 +1,14 @@
 export { convertCsvToJson } from "./converter.js";
 export { parseQuestionCsv } from "./csv.js";
+export { createTopicSummary } from "./topic-summary.js";
 export { validateQuestionDataset } from "./validator.js";
 export type { ConvertCsvToJsonOptions, ConvertCsvToJsonResult } from "./converter.js";
+export type {
+  CreateTopicSummaryOptions,
+  PracticeResultRecord,
+  TopicSummaryItem,
+  TopicSummaryResult
+} from "./topic-summary.js";
 export type { ValidateQuestionDatasetOptions } from "./validator.js";
 export type {
   CognitiveLevel,
